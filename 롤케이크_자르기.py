23ccf313b6faf77838249aeb82fc1c990aadc75c
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-10-21 13:57:48 +0900

    롤케이크_자르기 완료

--- 롤케이크_자르기.py
+++ 롤케이크_자르기.py
@@ -1,6 +1,26 @@
 #
+from collections import Counter
+
 def solution( topping ):
-	result = 2
+	result = 0
+
+	roll_1_count_dict = {}
+	roll_1_type_count = 0
+	roll_2_count_dict = Counter( topping )
+	roll_2_type_count = len( roll_2_count_dict.keys() )
+
+	for topping_item in topping:
+		if not topping_item in roll_1_count_dict:
+			roll_1_count_dict[ topping_item ] = 1
+			roll_1_type_count += 1
+
+		roll_2_count_dict[ topping_item ] = roll_2_count_dict[ topping_item ] - 1
+		if roll_2_count_dict[ topping_item ] == 0:
+			roll_2_type_count -= 1
+
+		if roll_1_type_count == roll_2_type_count:
+			result += 1
+
 	return result
 
 if __name__ == '__main__':
