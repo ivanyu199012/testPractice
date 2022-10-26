330425c59cf656130275b44fce7dc651084862e7
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-10-26 14:12:54 +0900

    min, max 함수 사용 해결 방법 추가

--- 최댓값과_최솟값.py
+++ 최댓값과_최솟값.py
@@ -2,21 +2,25 @@
 import heapq
 
 
-def solution( s ):
+# def solution( s ):
 
-	num_str_arr = s.split()
-	nums = [ int( num_str ) for num_str in num_str_arr ]
-	neg_nums = [ -1 * num for num in nums ]
+# 	num_str_arr = s.split()
+# 	nums = list( map( int, s.split() ) )
+# 	neg_nums = [ -1 * num for num in nums ]
 
-	heapq.heapify( nums )
-	heapq.heapify( neg_nums )
+# 	heapq.heapify( nums )
+# 	heapq.heapify( neg_nums )
 
-	min = heapq.heappop( nums )
-	max = -1 * heapq.heappop( neg_nums )
+# 	min = heapq.heappop( nums )
+# 	max = -1 * heapq.heappop( neg_nums )
 
-	return str( min ) + ' '  + str( max )
+# 	return str( min ) + ' '  + str( max )
+
+def solution(s):
+    arr = list(map(int, s.split(' ')))
+    return str(min(arr)) + " " + str(max(arr))
 
 if __name__ == '__main__':
 	print(f'{ solution( "1 2 3 4" )= }, ea = "1 4"')
-	# print(f'{ solution( "-1 -2 -3 -4" )= }, ea = "-4 -1"')
-	# print(f'{ solution( "-1 -1" )= }, ea = "-1 -1"')
+	print(f'{ solution( "-1 -2 -3 -4" )= }, ea = "-4 -1"')
+	print(f'{ solution( "-1 -1" )= }, ea = "-1 -1"')
