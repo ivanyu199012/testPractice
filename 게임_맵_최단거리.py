4f1e6835ba22a9136687680f549673675ff8af2f
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-11-03 13:17:38 +0900

    다른 사람 대답을 참고하여 재개발

--- 게임_맵_최단거리.py
+++ 게임_맵_최단거리.py
@@ -2,54 +2,30 @@
 from collections import deque
 
 def solution( maps ):
-	count = 0
 
-	row_max, col_max = len( maps ) - 1, len( maps[ 0 ]) - 1
-	not_visited_queue_arr = deque( [ deque( [ ( 0, 0, count ) ] ) ] )
-	visited_set = set()
+	row_amount, col_amount = len( maps ), len( maps[ 0 ])
+	not_visited_queue = deque( [ ( 0, 0 ) ] )
+	graph = [ [ -1 for _ in range( col_amount ) ] for _ in range( row_amount ) ]
 	directions = [ [ 0, 1 ], [ 1, 0 ], [ 0, -1 ], [ -1, 0 ]  ]
 
-	while len( not_visited_queue_arr ) > 0:
-		not_visited_queue = not_visited_queue_arr.popleft()
-		while len( not_visited_queue ) > 0:
-			x, y, count = not_visited_queue.popleft()
+	graph[ 0 ][ 0 ] = 1
+	while len( not_visited_queue ) > 0:
 
-			if ( x, y ) in visited_set:
-				break
-			visited_set.add( ( x, y ) )
-			count += 1
+		x, y = not_visited_queue.popleft()
 
-			if ( x, y ) == ( row_max, col_max ):
-				return count
+		for dx, dy in directions:
+			new_x = x + dx
+			new_y = y + dy
 
-			possible_directions = []
-			for dx, dy in directions:
-				new_x = x + dx
-				new_y = y + dy
+			if  0 <= new_x < row_amount and 0 <=  new_y < col_amount:
+				if maps[ new_x ][ new_y ] == 1 and graph[ new_x ][ new_y ] == -1:
+					not_visited_queue.append( ( new_x, new_y ) )
+					graph[ new_x ][ new_y ] = graph[ x ][ y ] + 1
 
-				if new_x < 0 or new_x > row_max:
-					continue
-				elif new_y < 0 or new_y > col_max:
-					continue
-				if maps[ new_x ][ new_y ] == 0:
-					continue
-				if ( new_x, new_y ) in visited_set:
-					continue
-				possible_directions.append( ( dx, dy ) )
+			if new_x == row_amount - 1 and new_y == col_amount - 1:
+				return graph[ new_x ][ new_y ]
 
-			if len( possible_directions ) == 0:
-				break
-
-			for dx, dy in possible_directions:
-				new_x = x + dx
-				new_y = y + dy
-
-				not_visited_queue_arr.append( deque( [ ( new_x, new_y, count ) ] ) )
-
-			break
-
-	return -1
-
+	return graph[ -1 ][ -1 ]
 
 if __name__ == '__main__':
 	result = solution( [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]] )
