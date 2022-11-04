99b7b9919579825147405cbb2e6d46f38185378f
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-11-04 17:00:23 +0900

    베스트앨범 완료

--- 베스트앨범.py
+++ 베스트앨범.py
@@ -7,7 +7,6 @@ def solution( genres, plays ):
 	genre_2_view_count_N_plays_dict ={}
 	for i in range( len( genres ) ):
 		genre = genres[i]
-
 		genre_2_view_count_N_plays_dict[ genre ] = genre_2_view_count_N_plays_dict.get( genre, { "view_count" : 0, "play_indexes" : [] } )
 		genre_2_view_count_N_plays_dict[ genre ][ "view_count" ] += plays[ i ]
 		genre_2_view_count_N_plays_dict[ genre ][ "play_indexes" ].append( i )
@@ -15,21 +14,10 @@ def solution( genres, plays ):
 	genre_list = list( genre_2_view_count_N_plays_dict.keys() )
 	genre_list.sort( key=lambda genre: genre_2_view_count_N_plays_dict[ genre ][ "view_count" ], reverse=True )
 
-	def compare_play( play1_index, play2_index ):
-		if plays[ play1_index ] == plays[ play2_index ]:
-			if play1_index < play2_index:
-				return 1
-			else:
-				return -1
-		else:
-			return plays[ play1_index ] - plays[ play2_index ]
-
-	compare_play_key = cmp_to_key( compare_play )
-
 	answer = []
 	for genre in genre_list:
 		play_indexes : list = genre_2_view_count_N_plays_dict[ genre ][ "play_indexes" ]
-		play_indexes.sort( key=compare_play_key, reverse=True )
+		play_indexes.sort( key=lambda play_i : ( -plays[ play_i ], play_i  ) )
 
 		if len( play_indexes ) >= 2:
 			answer += play_indexes[ :2 ]
